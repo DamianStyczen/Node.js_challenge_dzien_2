1af27637fa081f43caf5f7d3e7387dab95d92cf5
Styku0 <styku94@gmail.com>
2018-09-19 16:35:09 +0200

    Zadania rozwiązane

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,21 @@
 //Twój kod
+const fs = require("fs");
+
+fs.readFile("./data/zadanie01/input.json", "utf8", (err, data) =>{
+    if(err === null){
+        let resultArray = JSON.parse(data);
+        console.log(resultArray);
+        let sum = resultArray.reduce((a, b) => a+b);
+        console.log(sum);
+        fs.writeFile("./data/zadanie01/sum.txt", sum, err => {
+            if (err === null){
+                console.log('Zapisano poprawnie!');
+            } else {
+                console.log('Błąd podczas zapisu pliku!', err);
+            }
+        })
+    }
+    else{
+        console.log('Błąd podczas odczytu pliku!', err);
+    }
+})
--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,30 @@
 //Twój kod
+const fs = require("fs");
+
+const fileURL = process.argv[2];
+
+fs.readFile(fileURL, "utf8", (err, data)=>{
+    if(err === null){
+        let str = data.split("");
+        let resultString = "";
+        str.forEach((element, index) => {
+            
+            if(index%2 == 0){
+                element = element.toUpperCase();
+            }
+            resultString += element;
+            
+        })
+        fs.writeFile(fileURL, resultString, err =>{
+            if(err === null){
+                console.log("Zapisano poprawnie");
+            }
+            else{
+                console.log("Błąd zapisu pliku.", err);
+            }
+        })
+    }
+    else{
+        console.log("Błąd odczytu pliku.", err);
+    }
+})
